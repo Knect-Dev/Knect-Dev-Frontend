b6fc28e25231a8bfab71df495f0ca6b4a1187d98
Andrew Enyeart <reallyhardtoforget@gmail.com>
2022-03-14 16:53:51 -0700

    Adds floating action button to home page for adding new job

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,15 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../../components/ExploreContainer';
+import { IonContent, IonFab, IonFabButton, IonIcon, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+// import ExploreContainer from '../../components/ExploreContainer';
+import { addOutline } from 'ionicons/icons';
+
 import './Home.css';
 
 const Home = () => {
+
+  const handleClick = (e) => {
+    alert("You've got mayo.");
+  };
+
   return (
     <IonPage>
       <IonHeader>
@@ -10,14 +17,23 @@ const Home = () => {
           <IonTitle>Home</IonTitle>
         </IonToolbar>
       </IonHeader>
+
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Home</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Home page" />
+        {/* New content goes here */}
+        {/* <ExploreContainer name="Home page" /> */}
+         {/*-- fab placed to the top end --*/}
+         <IonFab vertical="top" horizontal="end" slot="fixed">
+          <IonFabButton onClick={handleClick}>
+            <IonIcon icon={addOutline} />
+          </IonFabButton>
+        </IonFab>
       </IonContent>
+
     </IonPage>
   );
 };
