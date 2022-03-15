f812437ac414d4322b12f12abe05b2fa0e416194
Daniel Jackson <daniel.jakob.jackson@gmail.com>
2022-03-15 12:11:24 -0600

    creates form for each different Model the User will want to input

---
+++ src/components/Form/Company/CompanyForm.jsx
@@ -0,0 +1,14 @@
+import { IonModal, IonContent, IonButton, IonItem, IonInput } from '@ionic/react';
+
+
+const CompanyForm = () => {
+  return (
+    <>
+      <IonItem>
+        <IonInput value={'woop'} placeholder="Enter Input" clearInput></IonInput>
+      </IonItem>
+    </>
+  )
+}
+
+export default CompanyForm;
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,26 +1,30 @@
 import { IonModal, IonContent, IonButton, IonItem, IonInput } from '@ionic/react';
 import { useState } from 'react';
 
+import JobForm from './Job/JobForm';
+import CompanyForm from './Company/CompanyForm';
+import ContactForm from './Contact/ContactForm';
+
 import './Form.scss';
 
 const Form = ({ showForm, setShowForm }) => {
 
   const [locked, setLocked] = useState(true);
+  const [activeForm, setActiveForm] = useState('job');
 
   return (
     <>
-      {/* <IonButton onClick={setShowModal(!showModal)}>Show</IonButton> */}
       <IonModal
         isOpen={showForm}
         swipeToClose={true}
         backdropDismiss={false}
       >
-        <IonContent>
-          <IonItem>
-            <IonInput value={'woop'} placeholder="Enter Input" clearInput></IonInput>
-          </IonItem>
-          <IonButton onClick={() => setShowForm(!showForm)}>Close</IonButton>
-        </IonContent>
+        <IonButton onClick={() => setShowForm(!showForm)}>Close</IonButton>
+        <div>
+          <IonButton>Job</IonButton>
+          <IonButton>Company</IonButton>
+          <IonButton>Contact</IonButton>
+        </div>
       </IonModal>
     </>
   )
