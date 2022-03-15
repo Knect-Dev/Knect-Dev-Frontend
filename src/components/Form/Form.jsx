b75787c2c8444e3e1219a9c21c26f75feb8d4f50
Daniel Jackson <daniel.jakob.jackson@gmail.com>
2022-03-15 10:53:52 -0600

    adding form layout

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,17 @@
+import { IonModal, IonContent, IonButton } from '@ionic/react';
+
+import './Form.scss';
+
+const Form = () => {
+  return (
+    <IonModal
+      isOpen={true}
+      swipeToClose={ }
+    >
+
+      <IonContent>Word</IonContent>
+    </IonModal>
+  )
+}
+
+export default Form;
