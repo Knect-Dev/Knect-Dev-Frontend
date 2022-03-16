3c3e4521459390df649f2b56ca511d570edff70a
Kellen-Linse <kblinse06@gmail.com>
2022-03-15 18:58:01 -0700

    hot fix for recent merge

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,19 @@
 import { useState } from 'react';
-import { IonHeader, IonTitle, IonToolbar, IonSearchbar, IonMenuButton, IonButton, IonButtons, IonToggle, IonIcon } from '@ionic/react';
+import {
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonSearchbar,
+  IonMenuButton,
+  IonButton,
+  IonButtons,
+  IonToggle,
+  IonIcon,
+} from '@ionic/react';
 import { sunnyOutline, moonOutline } from 'ionicons/icons';
 import { accessibilityOutline, contrastOutline } from 'ionicons/icons';
-import {  useSelector } from 'react-redux';
-import './header.scss'
+import { useSelector } from 'react-redux';
+import './header.scss';
 
 const Header = () => {
   const [searchText, setSearchText] = useState('');
@@ -14,41 +24,35 @@ const Header = () => {
     darkTheme ? setDarkTheme(false) : setDarkTheme(true);
   };
 
-  let userState = useSelector(state => state.user)
-
+  let userState = useSelector((state) => state.user);
 
   return (
-
     <IonHeader>
       <IonToolbar>
-        <IonButtons slot="start">
+        <IonButtons slot='start'>
           <IonButton>
-            <IonIcon slot="icon-only" icon={accessibilityOutline}></IonIcon>
+            <IonIcon slot='icon-only' icon={accessibilityOutline}></IonIcon>
           </IonButton>
-
-          {
-            darkTheme ?
-              <IonIcon icon={sunnyOutline} onClick={themeToggleHandler}></IonIcon>
-              :
-              <IonIcon icon={moonOutline} onClick={themeToggleHandler}></IonIcon>
-          }
-
-            slot="icon-only" icon={contrastOutline} class="themeToggle"
-            onIonChange={themeToggleHandler}
-          /> */}
+          {darkTheme ?
+            <IonIcon icon={sunnyOutline} onClick={themeToggleHandler}></IonIcon>
+            : 
+            <IonIcon icon={moonOutline} onClick={themeToggleHandler}></IonIcon>}
         </IonButtons>
         <IonTitle>Knect.Dev</IonTitle>
-        <IonButtons slot="secondary">
-          <IonSearchbar id="search-bar" value={searchText} onIonChange={e => setSearchText(e.detail.value)}></IonSearchbar>
-        </ IonButtons>
-        <IonButtons slot="end">
+        <IonButtons slot='secondary'>
+          <IonSearchbar
+            id='search-bar'
+            value={searchText}
+            onIonChange={(e) => setSearchText(e.detail.value)}
+          ></IonSearchbar>
+        </IonButtons>
+        <IonButtons slot='end'>
           Filters
-          <IonMenuButton auto-hide="false"></IonMenuButton>
+          <IonMenuButton auto-hide='false'></IonMenuButton>
         </IonButtons>
       </IonToolbar>
     </IonHeader>
   );
-}
+};
 
 export default Header;
-
