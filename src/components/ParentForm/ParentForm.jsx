acce931fc47bb63a965071bc896163d15ed76ee3
Daniel Jackson <daniel.jakob.jackson@gmail.com>
2022-03-16 12:53:10 -0600

    updating styling of Parent Form styling and funcitonality

--- src/components/ParentForm/ParentForm.jsx
+++ src/components/ParentForm/ParentForm.jsx
@@ -1,4 +1,4 @@
-import { IonModal, IonContent, IonButton, IonIcon, IonItem, IonInput, IonItemGroup } from '@ionic/react';
+import { IonModal, IonButton } from '@ionic/react';
 import { useState, useReducer } from 'react';
 import { When } from 'react-if';
 
@@ -7,16 +7,13 @@ import CompanyForm from './Company/CompanyForm';
 import ContactForm from './Contact/ContactForm';
 import { initialState, reducer } from './FormReducer.jsx';
 
-import { lockOpenOutline, lockClosedOutline } from 'ionicons/icons';
-
 import './ParentForm.scss';
 
 const Form = ({ showForm, setShowForm }) => {
 
   const [state, dispatch] = useReducer(reducer, initialState);
   const [activeForm, setActiveForm] = useState('Job');
-  console.log(activeForm);
-  console.log(activeForm === 'Job' && 'active')
+
   return (
     <>
       <IonModal
@@ -35,9 +32,9 @@ const Form = ({ showForm, setShowForm }) => {
           <ContactForm state={state} dispatch={dispatch} showForm={showForm} setShowForm={setShowForm} />
         </When>
         <div className='button-group'>
-          <IonButton size='large' class={`tab-button job-button ${activeForm === 'Job' && 'active'}`} onClick={() => setActiveForm('Job')}>Job</IonButton>
-          <IonButton size='large' class={`tab-button company-button ${(activeForm === 'Company' && 'active')}`} onClick={() => setActiveForm('Company')}>Company</IonButton>
-          <IonButton size='large' class={`tab-button contact-button ${(activeForm === 'Contact' && 'active')}`} onClick={() => setActiveForm('Contact')}>Contact</IonButton>
+          <IonButton class='tab-button job-button' id={activeForm === 'Job' && 'active'} onClick={() => setActiveForm('Job')}>Job</IonButton>
+          <IonButton class='tab-button company-button' id={activeForm === 'Company' && 'active'} onClick={() => setActiveForm('Company')}>Company</IonButton>
+          <IonButton class='tab-button contact-button' id={activeForm === 'Contact' && 'active'} onClick={() => setActiveForm('Contact')}>Contacts</IonButton>
         </div>
       </IonModal>
     </>
