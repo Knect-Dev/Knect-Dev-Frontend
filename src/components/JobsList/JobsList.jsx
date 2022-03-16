d4dcb5031482e67b1ead4a4051ce96b6bedc6a7b
Andrew Enyeart <reallyhardtoforget@gmail.com>
2022-03-15 17:06:32 -0700

    Pivots from infinite scroll to conventional list

--- src/components/JobsList/JobsList.jsx
+++ src/components/JobsList/JobsList.jsx
@@ -2,75 +2,24 @@ import './jobsList.scss';
 
 import {
   IonContent,
-  IonInfiniteScroll,
-  IonInfiniteScrollContent,
   IonItem,
   IonLabel,
-  IonList,
-  useIonViewWillEnter
-} from '@ionic/react';
+  IonList} from '@ionic/react';
 import { useState } from 'react';
 
-const JobsList = () => {
-  const [data, setData] = useState([]); // use jobs data from redux state as initial state
-  const [isInfiniteDisabled, setInfiniteDisabled] = useState(false);
-
-  const pushData = () => { // This adds to current data set
-    const newData = [];
-    for (let i = 0; i < 20; i++) {
-      newData.push('#' + (i + 1)); // creates an item
-    }
-
-    // const max = data.length + 20;
-    // const min = max - 20;
-    // const newData = [];
-    // for (let i = min; i < max; i++) {
-    //   newData.push('#' + (i+1)); // creates an item
-    // }
-
-    setData([
-      ...data,
-      ...newData
-    ]);
-  }
-
-  const loadData = (ev) => { // gets called by `onIonInfinite`
-    setTimeout(() => {
-      pushData();
-      console.log('Loaded data');
-      ev.target.complete();
-      if (data.length === 1000) { // TODO set this to equal to the total jobs 
-        setInfiniteDisabled(true);
-      }
-    }, 500);
-  }
-
-  useIonViewWillEnter(() => { // initial rendering
-    pushData(); // use INITIAL data
-  });
-
+const JobsList = ({jobs}) => {
+ 
   return (
     <IonContent>
       <IonList class="ion-margin-">
-        {data.map((item, index) => {
+        {jobs.map((job, index) => {
           return (
             <IonItem class="ion-justify-content-center ion-margin-bottom" key={index}>
-              <IonLabel>{item}</IonLabel>
+              <IonLabel>{job}</IonLabel>
             </IonItem>
           )
         })}
       </IonList>
-
-      <IonInfiniteScroll
-        onIonInfinite={loadData} // 
-        threshold="100px"
-        disabled={isInfiniteDisabled}
-      >
-        <IonInfiniteScrollContent
-          loadingSpinner="bubbles"
-          loadingText="Loading more data..."
-        ></IonInfiniteScrollContent>
-      </IonInfiniteScroll>
     </IonContent>
   );
 };
