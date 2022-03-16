4d829e522cb0c7aef93e1d80d6c82f152b8017a9
SpencerTower <spencerjtower1@gmail.com>
2022-03-16 16:40:11 -0700

    reverts JobsList to render without useSelector

--- src/components/JobsList/JobsList.jsx
+++ src/components/JobsList/JobsList.jsx
@@ -3,12 +3,12 @@ import {
   IonContent,
   IonList
 } from '@ionic/react';
-import { useSelector } from 'react-redux';
 import JobItem from '../JobItems/JobItem';
 
-const JobsList = () => {
-  let jobState = useSelector((state) => state.jobs.jobs);
-  // console.log('JOBSTATE: ', jobState);
+const JobsList = ({ jobs }) => {
+  // let jobState = useSelector((state) => state.jobs.jobs); // This should replace the line below vvv
+  let jobState = jobs; // TEMP placeholder data until redux initial state created 
+  console.log('JOBSTATE: ', jobState);
 
   function handleClick(e) { // BUG more info: https://www.educative.io/edpresso/what-is-typeerror-converting-circular-structure-to-json
     let something = JSON.stringify(e.target);
