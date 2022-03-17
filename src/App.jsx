fe805c631d65d1be6b405576392a6c34a6a1aaee
erik savage <erik.k.savage@gmail.com>
2022-03-17 15:32:42 -0700

    breaks App into App and Main components to allow for conditional rendering of auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,6 @@
-import { Redirect, Route } from 'react-router-dom';
 import { Provider } from 'react-redux'; // redux
 import createReduxStore from './store';
-import {
-  IonApp,
-  IonIcon,
-  IonLabel,
-  IonRouterOutlet,
-  IonTabBar,
-  IonTabButton,
-  IonTabs,
-  setupIonicReact,
-  IonContent, IonMenu, IonList, IonItem
-} from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import { homeOutline, personCircleOutline, statsChartOutline } from 'ionicons/icons';
-import Home from './pages/Home/Home';
-import Stats from './pages/Stats/Stats';
-import Profile from './pages/Profile/Profile';
-import Header from './components/Header/Header';
-import Filter from './components/Filter/Filter';
+import { IonApp, setupIonicReact} from '@ionic/react';
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
 
@@ -35,14 +17,11 @@ import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
 
-import Auth from '../src/components/Auth/Auth';
-
 
 /* Theme variables */
 import './theme/variables.css';
 import './theme/font.scss';
-
-let isAuthed = false;
+import Main from './Main';
 
 setupIonicReact();
 const store = createReduxStore();
@@ -52,45 +31,7 @@ const App = () => {
   return(
   <Provider store={store}>
     <IonApp>
-      <Header />
-      <Filter />
-      <IonContent id="main">
-        {!isAuthed ?
-        <Auth/> :
-        <IonReactRouter>
-          <IonTabs>
-            <IonRouterOutlet>
-              <Route exact path="/home">
-                <Home />
-              </Route>
-              <Route exact path="/stats">
-                <Stats />
-              </Route>
-              <Route path="/profile">
-                <Profile />
-              </Route>
-              <Route exact path="/">
-                <Redirect to="/home" />
-              </Route>
-            </IonRouterOutlet>
-            <IonTabBar slot="bottom">
-              <IonTabButton tab="home" href="/home">
-                <IonIcon icon={homeOutline} />
-                <IonLabel>Home</IonLabel>
-              </IonTabButton>
-              <IonTabButton tab="stats" href="/stats">
-                <IonIcon icon={statsChartOutline} />
-                <IonLabel>Stats</IonLabel>
-              </IonTabButton>
-              <IonTabButton tab="profile" href="/profile">
-                <IonIcon icon={personCircleOutline} />
-                <IonLabel>Profile</IonLabel>
-              </IonTabButton>
-            </IonTabBar>
-          </IonTabs>
-        </IonReactRouter>
-        }
-      </IonContent>
+      <Main />
     </IonApp>
   </Provider>
   );
---
+++ src/Main.jsx
@@ -0,0 +1,71 @@
+import { Redirect, Route } from 'react-router-dom';
+import {
+  IonIcon,
+  IonLabel,
+  IonRouterOutlet,
+  IonTabBar,
+  IonTabButton,
+  IonTabs,
+  IonContent,
+} from '@ionic/react';
+import { homeOutline, personCircleOutline, statsChartOutline } from 'ionicons/icons';
+import { IonReactRouter } from '@ionic/react-router';
+import { useSelector } from 'react-redux';
+
+import Home from './pages/Home/Home';
+import Stats from './pages/Stats/Stats';
+import Profile from './pages/Profile/Profile';
+import Header from './components/Header/Header';
+import Filter from './components/Filter/Filter';
+import Auth from './components/Auth/Auth';
+
+const Main = () => {
+  
+  const user = useSelector(state => state.user);
+  
+  return(
+    <>
+      <Header />
+      <Filter />
+      <IonContent id="main">
+        {!user.user.email ?
+        <Auth/> :
+        <IonReactRouter>
+          <IonTabs>
+            <IonRouterOutlet>
+              <Route exact path="/home">
+                <Home />
+              </Route>
+              <Route exact path="/stats">
+                <Stats />
+              </Route>
+              <Route path="/profile">
+                <Profile />
+              </Route>
+              <Route exact path="/">
+                <Redirect to="/home" />
+              </Route>
+            </IonRouterOutlet>
+            <IonTabBar slot="bottom">
+              <IonTabButton tab="home" href="/home">
+                <IonIcon icon={homeOutline} />
+                <IonLabel>Home</IonLabel>
+              </IonTabButton>
+              <IonTabButton tab="stats" href="/stats">
+                <IonIcon icon={statsChartOutline} />
+                <IonLabel>Stats</IonLabel>
+              </IonTabButton>
+              <IonTabButton tab="profile" href="/profile">
+                <IonIcon icon={personCircleOutline} />
+                <IonLabel>Profile</IonLabel>
+              </IonTabButton>
+            </IonTabBar>
+          </IonTabs>
+        </IonReactRouter>
+        }
+      </IonContent>
+    </>
+  );
+};
+
+export default Main;
