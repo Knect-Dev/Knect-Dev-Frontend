9565970cfc73e82d83a361e6e4e55c9109098243
erik savage <erik.k.savage@gmail.com>
2022-03-17 14:34:02 -0700

    makes axios request on sign-in

--- src/store/user.js
+++ src/store/user.js
@@ -1,25 +1,24 @@
 import axios from 'axios';
 
 // Backend
-const USER_URL = ''
+const USER_URL = 'https://knect-dev.herokuapp.com/signin/'
 
 const initialState = {
-  user: [{
+  user: {
     firstName: 'Mya',
     lastName: 'Linse',
     email: 'MyaMooCow@takinNaps.com',
     token: '123abc',
     role: 'Queen of the World'
-  }],
+  },
 }
 
 const userReducer = ( state = initialState, action) => {
   let {type, payload} = action;
   
   switch (type) {
-
     case 'SET_USER':
-
+      //take data 
       return { user: payload.results};
 
       
@@ -49,6 +48,40 @@ export const getusers = async dispatch => {
     console.log(e)
   }
 }
+
+export const signInUser = (credientials) => async dispatch => {
+  //axios request to sign in a user
+  console.log('CREDS from SignINuser', credientials);
+  //encrypt the credentials
+
+  try {
+    let response = await axios({
+      url: USER_URL,
+      method: 'post',
+      auth: {
+        username: credientials.email,
+        password: credientials.password
+      }
+    });
+    console.log(response);
+    // let data = response.data;
+    // dispatch(setuser(data));
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+export const signUpUser = async dispatch => {
+  //axios request to sign up a user
+  try {
+    let response = await axios.get(USER_URL);
+    let data = response.data;
+    dispatch(setuser(data));
+  } catch (e) {
+    console.log(e)
+  }
+}
+
 
 
 export default userReducer;
