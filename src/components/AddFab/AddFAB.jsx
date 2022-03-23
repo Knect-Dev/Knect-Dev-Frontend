6f392b302e76dbe43ceae850b4521209bfffe743
Daniel Jackson <daniel.jakob.jackson@gmail.com>
2022-03-23 16:47:15 -0500

    FAB changed to open menu of add options

--- src/components/AddFab/AddFAB.jsx
+++ src/components/AddFab/AddFAB.jsx
@@ -1,18 +1,25 @@
-import { IonFab, IonFabButton, IonIcon } from '@ionic/react';
+import { IonFab, IonFabButton, IonIcon, IonPopover, IonContent, IonButton, IonItem, IonChip } from '@ionic/react';
 import { addOutline } from 'ionicons/icons';
 
 const AddFAB = ({ showForm, setShowForm, setSelectedJobId, setSelectedCompanyId, setActiveForm }) => {
 
-  const handleClick = () => {
-    setActiveForm('Job');
+  const handleClick = (input) => {
+    setActiveForm(input);
     setSelectedCompanyId(null);
     setSelectedJobId(null);
-    setShowForm(!showForm);
+    setShowForm(true);
   };
 
   return (
     <IonFab vertical="top" horizontal="end" slot="fixed">
-      <IonFabButton onClick={handleClick} color="success">
+      <IonFabButton id="add-trigger" color="success">
+        <IonPopover trigger="add-trigger">
+          <IonContent>
+            <IonItem onClick={() => handleClick('Job')} button={true} detail={true}><h3 style={{ margin: 'auto' }}>Add Job</h3></IonItem>
+            <IonItem onClick={() => handleClick('Company')} button={true} detail={true}><h3 style={{ margin: 'auto' }}>Add Company</h3></IonItem>
+            <IonItem onClick={() => handleClick('Contact')} button={true} detail={true}><h3 style={{ margin: 'auto' }}>Add Contact</h3></IonItem>
+          </IonContent>
+        </IonPopover>
         <IonIcon icon={addOutline} />
       </IonFabButton>
     </IonFab>
